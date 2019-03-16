eb66fe09eecbe49d8a16e2c02406e79ea42fce7b
tepmodiba <tepmodiba@gmail.com>
2019-03-17 00:43:11 +0200

    Setup

---
+++ app/src/main/java/com/dynamicminds/ffwsafetyalert/ArticleDetails.java
@@ -0,0 +1,13 @@
+package com.dynamicminds.ffwsafetyalert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArticleDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article_details);
+    }
+}
