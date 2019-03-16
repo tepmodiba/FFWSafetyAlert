82dc6f6771aecb765bb6c192d8ffe1708a6ae70c
tepmodiba <tepmodiba@gmail.com>
2019-03-17 00:45:24 +0200

    Setup

--- app/src/main/java/com/dynamicminds/ffwsafetyalert/MainActivity.java
+++ app/src/main/java/com/dynamicminds/ffwsafetyalert/MainActivity.java
@@ -76,17 +76,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    //mTextMessage.setText(R.string.title_home);
                     setFragment(homeFragment);
                     return true;
                 case R.id.navigation_dashboard:
-                    //mTextMessage.setText(R.string.title_dashboard);
                     setFragment(articleFragment);
-                    //getArticles();
 
                     return true;
                 case R.id.navigation_notifications:
-                    //mTextMessage.setText(R.string.title_notifications);
                     setFragment(notificationFragment);
                     return true;
             }
